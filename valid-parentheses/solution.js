dca407538baa535f2844ac053fc91bbba7de6670
Anastasia Kuperberg <kuperberg.anastasia@gmail.com>
2023-10-22 09:52:40 +0200

    solution 1

--- valid-parentheses/solution.js
+++ valid-parentheses/solution.js
@@ -4,6 +4,12 @@
  */
 var isValid = function (s) {
     for (let i = 0; i < s.length; i++) {
-        if (s[i] !== '(' || s[i] !== ')' || s[i] !== '[' || s[i] !== ']' || s[i] !== '{' || s[i] !== '}') { return true } else { return true };
+        if (s[i] !== '(' && s[i] !== ')' && s[i] !== '[' && s[i] !== ']' && s[i] !== '{' && s[i] !== '}') { return false };
+        if (s[i] === '(' && s[i + 1] !== ')') { return false } 
+        else if (s[i] === '{' && s[i + 1] !== '}') { return false } 
+        else if (s[i] === '[' && s[i + 1] !== ']') { return false } 
+        else {return true}
     }
 };
+
+console.log(isValid('()'))
