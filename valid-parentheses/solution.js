656d9777eb7dc97e37413d8b7af62f918f483721
Anastasia Kuperberg <kuperberg.anastasia@gmail.com>
2023-11-05 16:49:02 +0100

    solution 2

--- valid-parentheses/solution.js
+++ valid-parentheses/solution.js
@@ -1,13 +1,22 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function (s) {
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] !== '(' && s[i] !== ')' && s[i] !== '[' && s[i] !== ']' && s[i] !== '{' && s[i] !== '}') { return false };
-        if (s[i] === '(' && s[i + 1] !== ')') { return false } 
-        else if (s[i] === '{' && s[i + 1] !== '}') { return false } 
-        else if (s[i] === '[' && s[i + 1] !== ']') { return false } 
-        else {return true}
+var isValid = function(s) {
+    const parenthesisArray = [];
+
+  for (let i = 0; i < s.length; i += 1) {
+    const topOfArray = parenthesisArray[parenthesisArray.length - 1];
+
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+      parenthesisArray.push(s[i]);
+    } else if (s[i] === ')' && topOfArray === '(' && parenthesisArray.length !== 0) {
+      parenthesisArray.pop();
+    } else if (s[i] === ']' && topOfArray === '[' && parenthesisArray.length !== 0) {
+      parenthesisArray.pop();
+    } else if (s[i] === '}' && topOfArray === '{' && parenthesisArray.length !== 0) {
+      parenthesisArray.pop();
+    } else {
+      return false;
     }
+  }
+  if (parenthesisArray.length !== 0) {
+    return false;
+  } else return true;
 };
